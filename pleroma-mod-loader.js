8b2158239a2013c8cde407b87d68b244181f542e
Absturztaube <me@absturztau.be>
2019-05-08 23:52:27 +0200

    initial

---
+++ pleroma-mod-loader.js
@@ -0,0 +1,39 @@
+
+function PleromaModLoader () {
+    
+    this.config = {
+        'modDirectory': './instance/pleroma-mods/',
+        'mods': [
+            'catify',
+            'custom-styles',
+        ]
+    };
+    
+    this.loadedMods = [];
+}
+
+PleromaModLoader.prototype.loadMods = function() {
+    var self = this;
+    for(var mod of self.config.mods) {
+        var modPath = self.config.modDirectory + 'pleroma-mod-' + mod + ".js";
+        console.log('load ' + mod);
+        import(modPath) 
+            .then(module => {
+                console.log(module);
+                self.loadedMods.push(module.default());
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+}
+
+PleromaModLoader.prototype.run = function() {
+    var self = this;
+    for(var mod of self.loadedMods) {
+        this.running.push(mod());
+    }
+}
+
+window.pleromaModLoader = new PleromaModLoader();
+window.pleromaModLoader.loadMods();
